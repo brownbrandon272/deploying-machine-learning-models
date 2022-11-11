1fa2d4d52a37aba97a06408bea523220a7036738
Brandon Brown <brandon.brown@mulberryrisk.com>
2022-11-11 13:44:38 +0000

    Changes to be committed: 	new file:   assignment-05-BB/MANIFEST.in 	new file:   assignment-05-BB/classification_model/VERSION 	new file:   assignment-05-BB/classification_model/__init__.py 	new file:   assignment-05-BB/classification_model/config.yml 	new file:   assignment-05-BB/classification_model/config/__init__.py 	new file:   assignment-05-BB/classification_model/config/core.py 	new file:   assignment-05-BB/classification_model/datasets/__init__.py 	new file:   assignment-05-BB/classification_model/pipeline.py 	new file:   assignment-05-BB/classification_model/predict.py 	new file:   assignment-05-BB/classification_model/processing/__init__.py 	new file:   assignment-05-BB/classification_model/processing/data_manager.py 	new file:   assignment-05-BB/classification_model/processing/features.py 	new file:   assignment-05-BB/classification_model/processing/validation.py 	new file:   assignment-05-BB/classification_model/train_pipeline.py 	new file:   assignment-05-BB/classification_model/trained_models/__init__.py 	new file:   assignment-05-BB/mypy.ini 	new file:   assignment-05-BB/pyproject.toml 	new file:   assignment-05-BB/requirements/requirements.txt 	new file:   assignment-05-BB/requirements/test_requirements.txt 	new file:   assignment-05-BB/setup.py 	new file:   assignment-05-BB/tests/__init__.py 	new file:   assignment-05-BB/tests/conftest.py 	new file:   assignment-05-BB/tests/test_features.py 	new file:   assignment-05-BB/tests/test_prediction.py 	new file:   assignment-05-BB/tox.ini

---
+++ assignment-05-BB/classification_model/processing/features.py
@@ -0,0 +1,110 @@
+from typing import List
+
+import pandas as pd
+import numpy as np
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class FeatureTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self, verbose = False):
+        """
+        Constructor Function.
+        Because I will be using this in a pipeline, __init__() is
+        called when pipeline is first created.
+        """
+        self.config = Config()
+        self.verbose = verbose
+
+    def fit(self, X: pd.DataFrame, y: pd.Series = None):
+        return self
+
+    def transform(self, X: pd.DataFrame, y: pd.Series = None):
+        self.data = X.copy()
+        self.createMissing()
+        self.createTitle()
+        self.createCabin()
+        return self.data
+
+    def selectFeatures(self):
+        cols = self.config.cat_vars + self.config.cont_vars
+        self.data = self.data[cols]
+        return self
+        
+    def createMissing(self):
+        for x in self.config.missing:
+            self.data['Missing_' + x] = self.data[x].isna() + 0
+        return self
+
+    def createTitle(self):
+        ## Name -> Title
+        self.data['Title'] = \
+            self.data['Name'].apply(lambda x: x[x.find(',')+2 : x.find('.')])
+        return self
+
+    def createCabin(self):
+        def _get_cabin_number(cabin):
+            try:
+                return int(cabin.split()[-1][1:])
+            except:
+                return None
+        ## Cabin -> Cabin Letter and Cabin Number
+        self.data['CabinLetter'] = self.data['Cabin'].str.slice(0,1)
+        self.data['CabinNumber'] = self.data['Cabin'].apply(_get_cabin_number)
+        return self
+        
+    def labelEncoding(self):
+        for col, enc_dict in zip(self.config.ENC_DICTS.keys(), self.config.ENC_DICTS.values()):
+            self.data[col] = self.data[col] \
+                .map(enc_dict) \
+                .fillna(0) \
+                .replace({-1: np.nan})
+        return self
+
+
+class TemporalVariableTransformer(BaseEstimator, TransformerMixin):
+    """Temporal elapsed time transformer."""
+
+    def __init__(self, variables: List[str], reference_variable: str):
+
+        if not isinstance(variables, list):
+            raise ValueError("variables should be a list")
+
+        self.variables = variables
+        self.reference_variable = reference_variable
+
+    def fit(self, X: pd.DataFrame, y: pd.Series = None):
+        # we need this step to fit the sklearn pipeline
+        return self
+
+    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+
+        # so that we do not over-write the original dataframe
+        X = X.copy()
+
+        for feature in self.variables:
+            X[feature] = X[self.reference_variable] - X[feature]
+
+        return X
+
+
+class Mapper(BaseEstimator, TransformerMixin):
+    """Categorical variable mapper."""
+
+    def __init__(self, variables: List[str], mappings: dict):
+
+        if not isinstance(variables, list):
+            raise ValueError("variables should be a list")
+
+        self.variables = variables
+        self.mappings = mappings
+
+    def fit(self, X: pd.DataFrame, y: pd.Series = None):
+        # we need the fit statement to accomodate the sklearn pipeline
+        return self
+
+    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        X = X.copy()
+        for feature in self.variables:
+            X[feature] = X[feature].map(self.mappings)
+
+        return X
